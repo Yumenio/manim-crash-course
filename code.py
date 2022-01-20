a11b86c0ad32abe2051ba2b186051baf1c22c40c
Yumenio <alejandroandy447@gmail.com>
2022-01-20 03:18:14 -0500

    square to circle transition using 2 builtin magic functions

--- code.py
+++ code.py
@@ -6,3 +6,14 @@ class CreateCircle(Scene):
     circle.set_fill(RED, opacity = 0.5)
     self.play(Create(circle))
 
+class SquareToCircle(Scene):
+  def construct(self):
+    circle = Circle()
+    circle.set_fill(RED, opacity = 0.7)
+
+    square = Square()
+    square.rotate(PI/4)
+    
+    self.play(Create(square))
+    self.play(Transform(square,circle))
+    self.play(FadeOut(square))
