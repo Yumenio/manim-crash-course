3af1c126157f5fe4964c9b753f71789012b10f0a
Yumenio <alejandroandy447@gmail.com>
2022-01-20 23:37:08 -0500

    placing square next to circle

--- code.py
+++ code.py
@@ -17,3 +17,15 @@ class SquareToCircle(Scene):
     self.play(Create(square))
     self.play(Transform(square,circle))
     self.play(FadeOut(square))
+
+
+class SquareNextToCircle(Scene):
+  def construct(self):
+    circle = Circle()
+    circle.set_fill(RED, opacity=0.7)
+
+    square = Square()
+    square.rotate(PI/4)
+    
+    square.next_to(circle, RIGHT, buff=-0.5)
+    self.play(Create(square), Create(circle))
