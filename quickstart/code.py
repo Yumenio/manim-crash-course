fe999191ec13c3e1379f6d2ef8150842d44acb01
Yumenio <alejandroandy447@gmail.com>
2022-01-20 23:45:38 -0500

    ...

--- quickstart/code.py
+++ quickstart/code.py
@@ -29,3 +29,13 @@ class SquareNextToCircle(Scene):
     
     square.next_to(circle, RIGHT, buff=-0.5)
     self.play(Create(square), Create(circle))
+
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
